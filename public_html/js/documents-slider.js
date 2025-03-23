940d2337daeee68330abb0fd90c6f0fe588e2da1
Lench228 <leonidgneusev@gmail.com>
2025-03-23 20:42:13 +0500

    fix: optimized images, imports, acessability

--- public_html/js/documents-slider.js
+++
@@ -1,8 +0,0 @@
-const swiper = new Swiper('.mySwiper', {
-    slidesPerView: 3,
-    spaceBetween: 10,
-      navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
